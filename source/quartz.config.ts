25c051e0d5b873a50e7db8635232a31d2dedff17
Rin Takahashi <131187346+rin-bell4772@users.noreply.github.com>
2025-02-18 19:13:05 -0500

    Update quartz.config.ts : changed light mode colors

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -29,14 +29,14 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          light: "#eff1f5",
+          lightgray: "#9ca0b0",
+          gray: "#7287fd",
+          darkgray: "#4c4f69",
+          dark: "#ea76cb",
+          secondary: "#e64553",
+          tertiary: "#d20f39",
+          highlight: "rgba(114, 135, 253, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
